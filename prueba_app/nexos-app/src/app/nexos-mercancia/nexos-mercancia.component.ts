40c8ee23d27157094a7b9dc380ccbb10039e03fd
Criscamg <ccgonzalezd@gmail.com>
2025-07-07 08:50:25 -0500

    filtro tabla

--- prueba_app/nexos-app/src/app/nexos-mercancia/nexos-mercancia.component.ts
+++ prueba_app/nexos-app/src/app/nexos-mercancia/nexos-mercancia.component.ts
@@ -1,27 +1,36 @@
 import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { MercanciaService } from '../servicios/mercancia-service.service';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { MercanciaDto } from '../modelos/mercancia-dto';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input'
 
 
 
 @Component({
   selector: 'app-nexos-mercancia',
   standalone: true,
-  imports: [AsyncPipe, MatTableModule],
+  imports: [AsyncPipe, MatTableModule, CommonModule, MatInputModule],
   templateUrl: './nexos-mercancia.component.html',
   styleUrl: './nexos-mercancia.component.css'
 })
 export class NexosMercanciaComponent {
   public mercanciaList: MercanciaDto[];
-  constructor(private mercanciaService: MercanciaService){}
+  public dataSource: any;
+  constructor(private mercanciaService: MercanciaService
+  ){}
 
   ngOnInit(): void{
     this.mercanciaService.obtenerMercancias().subscribe(
       res=> this.mercanciaList = res);
+      this.dataSource = new MatTableDataSource(this.mercanciaList);
     console.log(this.mercanciaList);
   }
-  displayedColumns: string[] = ['id', 'Nombre Mercancia', 'weight', 'symbol'];
+  displayedColumns: string[] = ['id', 'Nombre Mercancia', 'Cantidad', 'Fecha registro'];
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 
 }
